98f940eac09baec78db70b4c373a387a18512fff
EfeDeveloper <edwinvilla_adsi@outlook.com>
2025-06-18 17:13:27 -0500

    ♻️refactor: update app configuration and routing
    - Removed unused styleUrl from AppComponent.
    - Refactored imports in app.config.ts for better readability and updated the routes import path.
    - Changed the GraphQL endpoint to use the environment variable.
    - Added NgRx store provider to the app configuration.
    - Updated routing to include CharactersPageComponent as the default route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CharactersPageComponent } from '@/features/characters/characters-page/characters-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CharactersPageComponent },
+];
