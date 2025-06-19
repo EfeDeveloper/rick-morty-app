5993183fb548e78172a2265eee87fdb6906767f1
EfeDeveloper <edwinvilla_adsi@outlook.com>
2025-06-19 02:14:07 -0500

    ✨[component]Refactor totals bar component layout and styling - Updated the template to use Angular Material's mat-chip-listbox for better UI consistency. - Enhanced the display of species and type totals with improved styling and layout. - Added color classes for chips to visually differentiate species and types.

--- src/app/features/totals-bar/totals-bar.component.ts
+++ src/app/features/totals-bar/totals-bar.component.ts
@@ -1,15 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-totals-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatChipsModule, MatCardModule],
   templateUrl: './totals-bar.component.html',
 })
 export class TotalsBarComponent {
   @Input() speciesTotals: { [key: string]: number } = {};
   @Input() typeTotals: { [key: string]: number } = {};
+
+  chipColors = [
+    '!bg-blue-200',
+    '!bg-green-200',
+    '!bg-yellow-200',
+    '!bg-red-200 ',
+    '!bg-purple-200',
+    '!bg-pink-200',
+    '!bg-gray-200',
+  ];
+
+  getColorClass(index: number) {
+    return this.chipColors[index % this.chipColors.length];
+  }
 
   get objectKeys() {
     return Object.keys;
